a2d24bc6dc963f15845fdaac5fd7cc2711f35b8b
Deven1902 <devenamazingnandapurkar@gmail.com>
2024-01-24 12:29:38 +0530

    solved

---
+++ index.js
@@ -0,0 +1,26 @@
+const people = ["Amit", "Rahul", "Sanjay"];
+
+const expenses = new Map(); 
+
+function randomPayer(parties) {
+    return people[Math.floor(Math.random() * people.length)];
+}
+
+for (let i = 1; i <= 10; i++) {
+    const expense = new Map();
+    const totalExpense = Math.floor(Math.random() * 1000) + 100; // Random expense between 100 and 1000
+    const payer = randomPayer(people);
+    for (const party of people) {
+        expense.set(party, totalExpense / people.length);
+    }
+    expenses.set(`Expense ${i}`, { payer, expense });
+    console.log(expense);
+}
+
+// for (const [expenseName, { payer, expense }] of expenses) {
+//     console.log(`${expenseName} - Payer: ${payer}`,);
+//     for (const [party, share] of expense) {
+//         console.log(`  ${party}: ${share}`,);
+//     }
+//     console.log('\n');
+// }
